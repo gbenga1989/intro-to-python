e1b9c0e309c9678c6d0588426b8cd9b48550a50d
gbenga <odetoyinbog@gmail.com>
2025-02-10 15:18:09 +0100

    10-2

--- myname.py
+++ myname.py
@@ -1,56 +1,85 @@
-print("my name is odetoyinbo gbenga")
-print("my gender is male")
+# print("my name is odetoyinbo gbenga")
+# print("my gender is male")
 
-myname = "odetoyinbo gbenga"
-mygender = "male"
+# myname = "odetoyinbo gbenga"
+# mygender = "male"
 
-print(f"my name is {myname}")
-print(f"my gender is {mygender}")
+# print(f"my name is {myname}")
+# print(f"my gender is {mygender}")
 
-print("my name is {}".format(myname))
-print("my gender is {}".format(mygender))
-
-
-#this is a single line comment
-
-"""
-this is a multiline comment
-this is a multiline comment
-this is a multiline comment
-this is a multiline comment
-this is a multiline comment
-"""
-
-age = 25
-my_indian_name = "viral soteriya"
-is_a_graduate = True
-
-print(age)
-print(my_indian_name)
-print(is_a_graduate)
+# print("my name is {}".format(myname))
+# print("my gender is {}".format(mygender))
 
 
-print(f"my name  is {my_indian_name}, and i am {age} years old, i am also a {is_a_graduate} graduate")
+# #this is a single line comment
 
-print(f"my age is {age}")
-print("my age is {}".format(age))
+# """
+# this is a multiline comment
+# this is a multiline comment
+# this is a multiline comment
+# this is a multiline comment
+# this is a multiline comment
+# """
 
-house = "myhouse"
-house_age = 50
-house_is_new = False
+# age = 25
+# my_indian_name = "viral soteriya"
+# is_a_graduate = True
+
+# print(age)
+# print(my_indian_name)
+# print(is_a_graduate)
 
 
-age = "20 years old"
-i_am_home = True
-losses_in_life = 20
-egusi_price = 49.99
+# print(f"my name  is {my_indian_name}, and i am {age} years old, i am also a {is_a_graduate} graduate")
+
+# print(f"my age is {age}")
+# print("my age is {}".format(age))
+
+# house = "myhouse"
+# house_age = 50
+# house_is_new = False
 
 
-print(age)
-print(i_am_home)
+# age = "20 years old"
+# i_am_home = True
+# losses_in_life = 20
+# egusi_price = 49.99
 
-print(type(age))
 
-name = "10"
-age = int(name)
-print(type(age))
+# print(age)
+# print(i_am_home)
+
+# print(type(age))
+
+# name = "10"
+# age = int(name)
+# print(type(age))
+
+# 1.
+# 2.True
+# 3.True
+# 4.print(type(var))
+# 5.my-var = 20
+# 6. car_name = "volvo"
+
+
+y = 2
+z = 5
+addition = z + y
+print(addition)
+
+subtraction = 2 -2 
+print(subtraction)
+
+division = 2 / 2
+print(division)
+
+multiplication = 2 * 2
+print (multiplication)
+
+
+quotient = 2//2
+print(quotient)
+
+remainder = 2 % 2
+print(remainder)
