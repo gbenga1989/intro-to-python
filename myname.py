ead09c3d0011faa4e9bb1afc1d9188bae4653763
gbenga <odetoyinbog@gmail.com>
2025-02-11 14:57:54 +0100

    11-1

--- myname.py
+++ myname.py
@@ -86,15 +86,40 @@ print(remainder)
 
 # To create a list without square bracket list()
 
-my_list = [1, 2, 3, 4, 5]
+# my_list = [1, 2, 3, 4, 5]
 
-my_list = [1.3, 3.5, 7.8]
+# my_list = [1.3, 3.5, 7.8]
+
+# string_list = [ "apple", "orange", "pawpaw"]
+
+# mised_list = [10, "rice", 3.142, true]
+
+# empty_list =[]
+
+
+# my_info = ["bulls eye", ["fish"], list(3.142)]
+
+
+fruits = [ "apple", "banana", "cherry"]
+print(fruits[0])
 
 string_list = [ "apple", "orange", "pawpaw"]
+print(string_list[0])
+print(string_list[-2])
 
-mised_list = [10, "rice", 3.142, true]
+my_list = [1.3, 3.5, 7.8]
+print(my_list[0])
+print(my_list[1])
+print(my_list[-2])
+print(my_list[-1])
 
-empty_list =[]
+numbers = [0, 1,2,3,4, 5, 6, 7, 8]
+print(numbers[2:5])
+print(numbers[:5])
+print(numbers[::2]) (every secibd element)
 
 
-my_info = ["bulls eye", ["fish"], list(3.142)]
+numbers = [1,2,3]
+numbers.append(4)
+numbers.extend({6,7,8,9,10})
+print(numbers)
